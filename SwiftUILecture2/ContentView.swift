4aa1786c62e6a176b04351bb421f28c138ede879
stleye <stleye@gmail.com>
2022-12-04 08:45:42 -0300

    Initial Commit

---
+++ SwiftUILecture2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUILecture2
+//
+//  Created by Sebastian Tleye on 04/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILecture2/SwiftUILecture2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILecture2App.swift
+//  SwiftUILecture2
+//
+//  Created by Sebastian Tleye on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILecture2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
